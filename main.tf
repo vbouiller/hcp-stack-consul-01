0512a0d3d61d2ddf162970035c31db7c6acda1c6
JoeStack <joern@hashicorp.com>
2023-12-07 14:59:35 +0100

    VPC peering only 1.1

--- main.tf
+++ main.tf
@@ -46,36 +46,6 @@ data "aws_ami" "ubuntu" {
 }
 
 
-# resource "aws_eip" "nat_gateway" {
-#   #vpc = true
-# }
-
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat_gateway.id
-#   subnet_id     = aws_subnet.dmz_subnet.id
-
-#   tags = {
-#     Name = "gw NAT"
-#   }
-
-#   # To ensure proper ordering, it is recommended to add an explicit dependency
-#   # on the Internet Gateway for the VPC.
-#   depends_on = [aws_internet_gateway.igw]
-# }
-
-
-# Network & Routing
-# VPC 
-
-# resource "aws_vpc" "hashicorp_vpc" {
-#   cidr_block           = var.network_address_space
-#   enable_dns_hostnames = "true"
-
-#   tags = {
-#     Name = "${var.name}-vpc"
-#   }
-# }
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.10.0"
--- output.tf
+++ output.tf
@@ -14,9 +14,33 @@
 #   value = aws_instance.web_nodes.*.private_ip
 # }
 
+
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
+  description = "AWS VPC ID"
 }
+
+output "vpc_cidr_block" {
+  value       = module.vpc.vpc_cidr_block
+  description = "AWS VPC CIDR block"
+}
+
+output "subnet_id" {
+  value       = module.vpc.public_subnets[0]
+  description = "AWS public subnet"
+}
+
+output "hcp_consul_cluster_id" {
+  value       = hcp_consul_cluster.main.cluster_id
+  description = "HCP Consul ID"
+}
+
+output "hcp_consul_security_group" {
+  value       = module.aws_hcp_consul.security_group_id
+  description = "AWS Security group for HCP Consul"
+}
+
+
 
 # output "ca_public_key" {
 #   value = tls_private_key.signing-key.public_key_openssh
