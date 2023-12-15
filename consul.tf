a5ce263299a766f5c1c95a8ca5b8b4f4c3513cf5
JoeStack <joern@hashicorp.com>
2023-12-15 12:53:29 +0100

    my_asg 1.1

--- consul.tf
+++ consul.tf
@@ -63,6 +63,16 @@ resource "aws_security_group" "my_asg" {
   vpc_id      = module.vpc.vpc_id
 }
 
+resource "aws_security_group_rule" "egress" {
+  security_group_id = aws_security_group.my_asg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]  
+}
+
 resource "aws_security_group_rule" "consul-api" {
   #count             = var.consul_enabled ? 1 : 0
   security_group_id = aws_security_group.my_asg.id
