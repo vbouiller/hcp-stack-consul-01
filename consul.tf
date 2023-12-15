f3b187f1f932e30e94039574deeb8ceca81ec38a
JoeStack <joern@hashicorp.com>
2023-12-15 12:43:25 +0100

    my_asg 1

--- consul.tf
+++ consul.tf
@@ -56,6 +56,59 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+resource "aws_security_group" "my_asg" {
+  name        = "my_asg"
+  description = "Allow my inbound traffic"
+  #vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "consul-api" {
+  #count             = var.consul_enabled ? 1 : 0
+  security_group_id = aws_security_group.my_asg.id
+  type              = "ingress"
+  from_port         = 8500
+  to_port           = 8503
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]  
+  #cidr_blocks       = [var.whitelist_ip]
+}
+
+resource "aws_security_group_rule" "consul-dns-tcp" {
+  #count             = var.consul_enabled ? 1 : 0
+  security_group_id = aws_security_group.my_asg.id
+  type              = "ingress"
+  from_port         = 8600
+  to_port           = 8600
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
+resource "aws_security_group_rule" "consul-dns-udp" {
+  #count             = var.consul_enabled ? 1 : 0
+  security_group_id = aws_security_group.my_asg.id
+  type              = "ingress"
+  from_port         = 8600
+  to_port           = 8600
+  protocol          = "udp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
+resource "aws_security_group_rule" "consul-sidecar" {
+  #count             = var.consul_enabled ? 1 : 0
+  security_group_id = aws_security_group.my_asg.id
+  type              = "ingress"
+  from_port         = 21000
+  to_port           = 21255
+  protocol          = "tcp"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
+
 // Consul client instance
 resource "aws_instance" "consul_client_web" {
   count                       = 3
